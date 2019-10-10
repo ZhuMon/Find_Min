4f02be93dc27b3a723839be3b7d62fc8e15c2f33
ZhuMon <richardlin0212@gmail.com>
2019-10-11 02:13:20 +0800

    Add thread

--- p1_F54051201.py
+++ p1_F54051201.py
@@ -1,16 +1,50 @@
 import sys
 from functions import func
+import threading
 
+class MyRange():
+    def __init__(self, x_min, x_max, y_min, y_max):
+        self.x_min = x_min
+        self.x_max = x_max
+        self.y_min = y_min
+        self.y_max = y_max
 
-def brute(x_min, x_max, y_min, y_max):
-    z_min = 9999
-    for x in range(x_min, x_max):
-        for y in range(y_min, y_max):
+        self.x_mid = (x_min+x_max)//2
+        self.y_mid = (y_min+y_max)//2 
+
+def fun_thread(t):
+    global z_min
+    for x in range(t.x_min, t.x_max):
+        for y in range(t.y_min, t.y_max):
             z = func(x,y)
             if z < z_min:
                 z_min = z
 
-    return round(z_min)
+    
+
+
+def brute(t):
+    """
+    input <MyRange> t
+    """
+    global z_min
+    z_min = 9999
+
+    t1 = MyRange(t.x_min, t.x_mid, t.y_min, t.y_mid)
+    t2 = MyRange(t.x_mid, t.x_max, t.y_mid, t.y_max)
+    a_th = threading.Thread(target=fun_thread, args=(t1,))
+    b_th = threading.Thread(target=fun_thread, args=(t2,))
+    a_th.start()
+    b_th.start()
+    a_th.join()
+    b_th.join()
+
+    return round(z_min,3)
+
+def hill_climbing(point):
+    for p in point:
+        p[0]
+        p[1]
 
 def main():
     infile = open(sys.argv[1], "r")
@@ -19,6 +53,9 @@ def main():
     x_max = int(lines[0].split(',')[1])
     y_min = int(lines[1].split(',')[0])
     y_max = int(lines[1].split(',')[1])
+
+    threshold = MyRange(x_min, x_max, y_min, y_max)
+
     init_num = int(lines[2])
     init_point = []
     for i in range(init_num):
@@ -26,8 +63,9 @@ def main():
         y = float(lines[3+i].split(',')[1])
         init_point.append([x,y])
 
-    z_min = brute(x_min, x_max, y_min, y_max)
-    print(z_min)
+    z_min = brute(threshold)
+    z_min_climb = hill_climbing(init_point)
+    # print(z_min)
     outfile = open(sys.argv[2], "w")
     outfile.write(str("{:.3f}".format(z_min)))
     
